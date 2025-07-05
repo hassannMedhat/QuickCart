be5c432ed27a1f95c3d9cc777c77923c8eaf944b
abonessma <hassan8med@gmail.com>
2025-07-06 00:52:13 +0300

    first commit

---
+++ models/user.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+    },
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    cartItems: {
+        type: Object,
+        default: {},
+    },  
+
+});
+
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
